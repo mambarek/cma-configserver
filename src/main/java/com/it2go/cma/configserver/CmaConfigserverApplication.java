b1977dd4a03a4d4acee1cce863bbdda816a52fac
Mohamed Ali Mbarek <mbarek@it-2go.de>
2021-01-27 21:02:54 +0100

    first checkin

---
+++ src/main/java/com/it2go/cma/configserver/CmaConfigserverApplication.java
@@ -0,0 +1,13 @@
+package com.it2go.cma.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmaConfigserverApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CmaConfigserverApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/it2go/cma/configserver/CmaConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.it2go.cma.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmaConfigserverApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
