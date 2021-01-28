8fac9b69b85cd18dd88cb6242d118e47a8509c6e
Mohamed Ali Mbarek <mbarek@it-2go.de>
2021-01-29 00:32:58 +0100

    eureka client added and github ssh instead of https

--- src/main/java/com/it2go/cma/configserver/CmaConfigServerApplication.java
+++ src/main/java/com/it2go/cma/configserver/CmaConfigServerApplication.java
@@ -2,12 +2,14 @@ package com.it2go.cma.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-public class CmaConfigserverApplication {
+@EnableConfigServer
+public class CmaConfigServerApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(CmaConfigserverApplication.class, args);
+    SpringApplication.run(CmaConfigServerApplication.class, args);
   }
 
 }
